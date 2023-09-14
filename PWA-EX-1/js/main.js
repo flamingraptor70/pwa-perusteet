9b03e47f9179a3da0eb688af60554d2a7f28bcba
Khai Chan <strubo.gt@gmail.com>
2023-09-14 19:56:24 +0300

    Added fonts

--- PWA-EX-1/js/main.js
+++ PWA-EX-1/js/main.js
@@ -1,8 +1,10 @@
-window.onload = () => {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('./sw.js');
+(async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const worker = await navigator.serviceWorker.register("./sw.js");
+      console.log("Service Worker Registered");
+    } catch (e) {
+      console.log(e.message);
     }
   }
+})();
